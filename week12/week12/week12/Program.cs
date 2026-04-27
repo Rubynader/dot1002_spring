24c73a41af520cf4e7df3df92e89d6d157778077
Rubynader <irmaksurmeli@posta.mu.edu.tr>
2026-04-27 10:28:38 +0300

    week 12 task and UML diagram

---
+++ week12/week12/week12/Program.cs
@@ -0,0 +1,26 @@
+﻿public class EnemyNPC
+{
+    public string Name { get; set; }
+    public int AttackPower { get; set; }
+}
+public class EnemyAI
+{
+    public void ChasePlayer(EnemyNPC enemy)
+    {
+        // AI Pathfinding algorithms to find and follow the player...
+    }
+}
+public class EnemyLootSystem
+{
+    public void DropLoot(EnemyNPC enemy)
+    {
+        // Spawning gold or items on the ground when the enemy dies...
+    }
+}
+public class EnemyUI
+{
+    public void RenderHealthBar(EnemyNPC enemy)
+    {
+        // Drawing UI elements on the screen to show enemy's remaining health...
+    }
+}
